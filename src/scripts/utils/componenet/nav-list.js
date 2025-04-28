51f7c8769160767f9bcd30a7b494c9c397ae3e8f
Muhamad Aprizal Hardi <aprizalrizalzal@gmail.com>
2025-04-28 12:21:14 +0800

    update

--- src/scripts/utils/componenet/nav-list.js
+++ src/scripts/utils/componenet/nav-list.js
@@ -7,7 +7,7 @@ export function generateUnauthenticatedNavList() {
 
 export function generateAuthenticatedNavList() {
     return `
-    <li><a href="#/add" id="add" id="new-story"><i class="fa-solid fa-plus"></i>Add New Story</a></li>
-    <li><a href="#/logout" id="logout">Keluar<i class="fa-duotone fa-solid fa-arrow-right-from-bracket"></i></a></li>
+    <li><a href="#/add" id="add" tabindex="0" id="new-story"><i class="fa-solid fa-plus"></i>Add New Story</a></li>
+    <li><a href="#/logout" tabindex="0" id="logout">Keluar<i class="fa-duotone fa-solid fa-arrow-right-from-bracket"></i></a></li>
     `;
 }
